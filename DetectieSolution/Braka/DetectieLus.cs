d35b2aadc052e9c62717f10e2969b7d61bcf1a47
unknown <pschmidt@pasit.nl>
2021-06-29 13:33:01 +0200

    Straat Final 2

--- DetectieSolution/Braka/DetectieLus.cs
+++ DetectieSolution/Braka/DetectieLus.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 
 namespace Braka
 {
+    public delegate void DetectHandler();
+
     public class DetectieLus
     {
         private List<IDetectable> devices = new List<IDetectable>();
+        public event DetectHandler Detecting;
 
         public void Connect(IDetectable device)
         {
@@ -16,11 +19,12 @@ namespace Braka
         {
             Console.WriteLine("De detectielus ziet iets");
 
+            Detecting?.Invoke();
             // Aangesloten devices worden geactiveerd
-            foreach (IDetectable device in devices)
-            {
-                device.Activate();
-            }
+            //foreach (IDetectable device in devices)
+            //{
+            //    device.Activate();
+            //}
             // Alternatief
             //for (int i = 0; i < devices.Count; i++)
             //{
