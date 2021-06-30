96e487aa79c8ec85f5baab69e91dc4fd3830b36b
unknown <pschmidt@pasit.nl>
2021-06-30 14:36:34 +0200

    wpf

--- WPFDemo/Calculator/ViewModels/CalculatorViewModel.cs
+++ WPFDemo/Calculator/ViewModels/CalculatorViewModel.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace Calculator.ViewModels
 {
-    public class CalculatorViewModel
+    public class CalculatorViewModel : BaseViewModel
     {
+        private int a;
+        private int b;
+        private int answer;
+
+        public int Answer
+        {
+            get { return answer; }
+            set { 
+                answer = value;
+                Notify(nameof(Answer));
+            }
+        }
+        public int B
+        {
+            get { return b; }
+            set 
+            { 
+                b = value;
+                Notify(nameof(B));
+            }
+        }
+        public int A
+        {
+            get { return a; }
+            set 
+            {
+                a = value;
+                Notify(nameof(A));
+            }
+        }
+
     }
 }
