3fc3215bc6ef94fd998338ac5ebcddccdc6dc93a
unknown <pschmidt@pasit.nl>
2021-06-30 14:28:23 +0200

    wpf

---
+++ WPFDemo/Calculator/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Calculator.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Notify(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WPFDemo/SammelDemo/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SammelDemo.Models
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set 
+            { 
+                _lastName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastName)));
+            }
+        }
+
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
